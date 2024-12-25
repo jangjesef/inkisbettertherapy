26091d19092dc3f63b817d499127041d245b0963
Jan Gajdoš <jangajdosprod@gmail.com>
2024-12-25 22:24:29 +0100

    funkční2

--- app/api/instagram/data-deletion/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function POST() {
-  // Zde by měla být logika pro smazání uživatelských dat
-  return NextResponse.json({ status: 'success' });
-} 
--- app/api/instagram/deauth/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function POST() {
-  // Zde by měla být logika pro odstranění tokenu
-  return NextResponse.json({ status: 'success' });
-} 
